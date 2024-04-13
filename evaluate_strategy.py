2698027b472226cb5d4beedc7b4cb0ca61e4b4cd
MatteoRBiner <102237520+MatteoRBiner@users.noreply.github.com>
2024-04-13 12:17:16 +0200

    Program for evaluating a single strategy

---
+++ evaluate_strategy.py
@@ -0,0 +1,15 @@
+import python_blackjack_simulator as bj
+
+"""
+define number of repetitions and action
+"""
+repetitions = 100000
+action = bj.hit_until_17
+
+if __name__ == "__main__":
+    total_reward = 0
+    for i in range(repetitions):
+        reward, _, _ = bj.game(action)
+        total_reward += reward
+    total_reward /= repetitions
+    print("Reward: " + str(total_reward))
