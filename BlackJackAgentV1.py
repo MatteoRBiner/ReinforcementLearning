e3cf5c337312961b028427abc34c9300782b1fe4
BeTha38 <102479143+BeTha38@users.noreply.github.com>
2024-05-23 18:31:34 +0200

    Update BlackJackAgentV1.py

--- BlackJackAgentV1.py
+++ BlackJackAgentV1.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import python_blackjack_simulator as bj
 import numpy as np
+import strategies
 
 
 class BlackJackAgentV1:
@@ -18,14 +19,14 @@ class BlackJackAgentV1:
         self.discount_factor = discount_factor
         self.training_error = []
 
-    def get_action(self, player_hand, dealer_hand):
+    def get_action(self, players_hand, player_hand, dealer_hand):
         if np.random.random() < self.epsilon:
             if np.random.random() < 0.5:
                 return 'h'
             else:
                 return 's'
         else:
-            if self.q_values[self.get_index(player_hand, dealer_hand)] > -0.007:
+            if self.q_values[self.get_index(players_hand, player_hand, dealer_hand)] > -0.007:
                 return 's'
             else:
                 return 'h'
@@ -33,7 +34,10 @@ class BlackJackAgentV1:
     def decay_epsilon(self):
         self.epsilon = max(self.final_epsilon, self.epsilon * self.epsilon_decay)
 
-    def get_index(self, player_hand, dealer_hand):
+    def get_index(self, players_hand, player_hand, dealer_hand):
+        other_players_value = 0
+        for hand in players_hand: 
+            other_players_value += bj.total(hand)
         player_value = bj.total(str(player_hand))
         dealer_value = bj.total(str(dealer_hand))
         has_ace = 0
@@ -44,19 +48,20 @@ class BlackJackAgentV1:
     def train(self, repetitions):
         total_reward = 0
         for i in range(repetitions):
-            reward, player_data, final_state = bj.game(self.get_action)
-            total_reward = (i * total_reward + reward) / (i+1)
-            if bj.total(final_state[0]) > 21:
-                self.q_values[self.get_index(final_state[0][:len(final_state[0])-1],final_state[1][0])] += self.lr*reward
-                self.q_values[self.get_index(final_state[0][:len(final_state[0])-1], final_state[1][0])] /= 2
+            reward, player_data, final_state = bj.game(self.get_action, strategies.expected_value_strategy)
+            total_reward = (i * total_reward + reward[0]) / (i+1)
+            if bj.total(final_state[0][0]) > 21:
+                self.q_values[self.get_index(final_state[0], final_state[0][0][:len(final_state[0][0])-1], final_state[1][0])] += self.lr*reward[0]
+                self.q_values[self.get_index(final_state[0], final_state[0][0][:len(final_state[0][0])-1], final_state[1][0])] /= 2
             else:
-                self.q_values[self.get_index(final_state[0], final_state[1][0])] += self.lr * reward
-                self.q_values[self.get_index(final_state[0], final_state[1][0])] /= 2
+                self.q_values[self.get_index(final_state[0], final_state[0][0], final_state[1][0])] += self.lr * reward[0]
+                self.q_values[self.get_index(final_state[0], final_state[0][0], final_state[1][0])] /= 2
             self.decay_epsilon()
 
         print(total_reward)
 
+repetitions = 10000
 
 if __name__ == "__main__":
     agent = BlackJackAgentV1(0.01, 0.5, 0.9, 0.001)
-    agent.train(1000)
+    agent.train(repetitions)
