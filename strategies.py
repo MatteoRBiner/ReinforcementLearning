6447aa29673f368427e30c272655b9a8d33ef819
BeTha38 <102479143+BeTha38@users.noreply.github.com>
2024-05-23 18:44:05 +0200

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -4,9 +4,9 @@ import numpy.random as rd
 
 def random_action(player_hands, player_hand, dealer_hand):
     if rd.random() < 0.5:
-        return 'h'
+        return "h"
     else:
-        return 's'
+        return "s"
 
 
 def naive_action(player_hands, player_hand, dealer_hand):
@@ -14,9 +14,9 @@ def naive_action(player_hands, player_hand, dealer_hand):
   Hit until bust.
   :param player_hand: list of cards
   :param dealer_hand: single card
-  :return: (action, empty)
+  :return: action
   """
-  return "h", ()
+  return "h"
 
 
 def hit_until_17(player_hands, player_hand, dealer_hand):
@@ -24,12 +24,12 @@ def hit_until_17(player_hands, player_hand, dealer_hand):
   Hit until score > 17 or bust
   :param player_hand: list of cards
   :param dealer_hand: single card
-  :return: (action, empty)
+  :return: action
   """
   action = "h"
   if bj.total(player_hand) >= 17:
     action = "s"
-  return action, ()
+  return action
 
 
 def expected_value_strategy(player_hands, player_hand, dealer_hand):
@@ -37,7 +37,7 @@ def expected_value_strategy(player_hands, player_hand, dealer_hand):
   Hit if (score + (expected value of next cared)) <= 21
   :param player_hand: list of cards
   :param dealer_hand: single card
-  :return: (action, empty)
+  :return: action
   """
   cards = 0
   value = 0
@@ -56,9 +56,9 @@ def expected_value_strategy(player_hands, player_hand, dealer_hand):
     else:
         expected_value_1 = (total_A1 - value - 1) / number_of_remaining_cards
     if bj.total(player_hand) + expected_value_1 <= 21:
-        return ("h", ())
+        return "h"
     else:
-        return ("s", ())
+        return "s"
   else:
     _ , used_aces = bj.total(player_hand, True)
     if used_aces == 1:
@@ -68,15 +68,15 @@ def expected_value_strategy(player_hands, player_hand, dealer_hand):
             expected_value_1 = (total_A1 + 10 - value - 1) / number_of_remaining_cards
         # Compare cases like: (A + 9 = 20 -> stand) and (A + 5 = 16 -> hit)
         if bj.total(player_hand) + expected_value_1 <= 21 or (bj.total(player_hand) + expected_value_1 - 10 <= 21 and bj.total(player_hand) + expected_value_1 - 10 > bj.total(player_hand)) or (bj.total(player_hand) + expected_value_1 - 10 <= 21 and bj.total(player_hand) + 2*expected_value_1 - 10 > bj.total(player_hand) and bj.total(player_hand) + 2*expected_value_1 - 10 <= 21):
-            return ("h", ())
+            return "h"
         else:
-            return ("s", ())
+            return "s"
     else:
         if not(dealer_hand == "A"):
             expected_value_1 = (total_A1 - value - bj.total(str(dealer_hand))) / number_of_remaining_cards
         else:
             expected_value_1 = (total_A1 - value - 1) / number_of_remaining_cards
         if bj.total(player_hand) + expected_value_1 <= 21:
-            return ("h", ())
+            return "h"
         else:
-            return ("s", ())
+            return "s"
