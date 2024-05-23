5679f5177a9d71338f0716d9d8a95f0a58e68028
BeTha38 <102479143+BeTha38@users.noreply.github.com>
2024-05-23 18:31:17 +0200

    Update evaluate_strategy.py

--- evaluate_strategy.py
+++ evaluate_strategy.py
@@ -5,15 +5,15 @@ import python_blackjack_simulator as bj
 """
 define number of repetitions and action
 """
-repetitions = 100000
+repetitions = 10000
 
 if __name__ == "__main__":
     total_reward = 0
     agent = ag.BlackJackAgentV1(0.01, 0.5, 0.9, 0.001)
-    agent.train(1000)
+    agent.train(repetitions)
     action = agent.get_action
     for i in range(repetitions):
-        reward, _, _ = bj.game(action)
-        total_reward += reward
+        reward, _, _ = bj.game(action, strategies.expected_value_strategy)
+        total_reward += reward[0]
     total_reward /= repetitions
     print("Reward: " + str(total_reward))
