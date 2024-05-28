25b7472cfaca0fede19cff2dc2406682a1bbc653
BeTha38 <102479143+BeTha38@users.noreply.github.com>
2024-05-29 01:24:40 +0200

    Update BlackJackAgentV2.py

--- BlackJackAgentV2.py
+++ BlackJackAgentV2.py
@@ -69,36 +69,50 @@ class BlackJackAgentV2:
         for i in range(repetitions):
             reward, player_data, final_state = bj.game(self.get_action, strategies.expected_value_strategy)
             total_reward += reward[0]
-            players_hand = final_state[0]
-            player_hand = final_state[0][0]
-            dealer_hand = final_state[1][0]
-            skip = False
-            if player_data[0] == []:
-                skip = True
-            elif player_data[-1] == "s":
-                action = 0
-            else:
-                action = 1
+            
+            if not(player_data[0] == []):
+                counter = 0
+                for action in player_data[0]:
+                    if action == "s":
+                        action_n = 0
+                    else:
+                        action_n = 1
+                    
+                    players_hand = final_state[0]
+                    player_hand = final_state[0][0][0:(1+counter)]
+                    dealer_hand = final_state[1][0]
 
-            other_players_sum = 0
-            for hand in players_hand: 
-                other_players_sum += bj.total(hand)
-            player_sum , used_aces = bj.total(player_hand, True)
-            has_ace = 0
-            for card in players_hand:
-                if card == 'A':
-                    has_ace += 1
+                    other_players_sum = 0
+                    for hand in players_hand: 
+                        other_players_sum += bj.total(hand[0:1])
+                    player_sum , used_aces = bj.total(player_hand, True)
+                    has_ace = 0
+                    for card in players_hand:
+                        if card == 'A':
+                            has_ace += 1
 
-            usable_aces = has_ace - used_aces
-            obs = tuple[player_sum, dealer_hand, usable_aces, other_players_sum]
-            if not(skip):
-                temporal_difference = (reward[0] + 0.95 * np.max(self.q_values[obs]) - self.q_values[obs][action])
-                self.q_values[obs][action] = (self.q_values[obs][action] + self.lr * temporal_difference)
-            self.decay_epsilon()
+                    usable_aces = has_ace - used_aces
+                    obs = tuple[player_sum, dealer_hand, usable_aces, other_players_sum]
+
+                    if action == "s":
+                        temporal_difference = (reward[0] - self.q_values[obs][action_n])
+                    else:
+                        player_hand = final_state[0][0][0:(2+counter)]
+                        player_sum , used_aces = bj.total(player_hand, True)
+                        has_ace = 0
+                        for card in players_hand:
+                            if card == 'A':
+                                has_ace += 1
+                        new_obs = tuple[player_sum, dealer_hand, usable_aces, other_players_sum]
+                        temporal_difference = (reward[0] + 0.95 * np.max(self.q_values[new_obs]) - self.q_values[obs][action_n])
+                    
+                    self.q_values[obs][action_n] = (self.q_values[obs][action_n] + self.lr * temporal_difference)
+                    self.decay_epsilon()
+                    counter += 1
 
         print(f"Average reward during training: {total_reward/i}")
 
 if __name__ == "__main__":
-    repetitions = 10000
-    agent = BlackJackAgentV2(0.001, 1, 2/repetitions, 0.1)
+    repetitions = 5000
+    agent = BlackJackAgentV2(0.01, 1, 2/repetitions, 0.1)
     agent.train(repetitions)
