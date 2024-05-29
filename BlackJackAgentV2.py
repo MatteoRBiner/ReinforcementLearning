67722a186efb12b4a7d77c802ec1685764aae595
BeTha38 <102479143+BeTha38@users.noreply.github.com>
2024-05-29 11:22:17 +0200

    Update BlackJackAgentV2.py

--- BlackJackAgentV2.py
+++ BlackJackAgentV2.py
@@ -65,19 +65,18 @@ class BlackJackAgentV2:
 
     def train(self, repetitions):
         total_reward = 0
-
-        for i in range(repetitions):
+        i = 0
+        counter1 = 0
+        counter2 = 0
+        counter3 = 0
+        counter4 = 0
+        while i < repetitions:
             reward, player_data, final_state = bj.game(self.get_action, strategies.expected_value_strategy)
             total_reward += reward[0]
             
             if not(player_data[0] == []):
                 counter = 0
                 for action in player_data[0]:
-                    if action == "s":
-                        action_n = 0
-                    else:
-                        action_n = 1
-                    
                     players_hand = final_state[0]
                     player_hand = final_state[0][0][0:(1+counter)]
                     dealer_hand = final_state[1][0]
@@ -90,29 +89,54 @@ class BlackJackAgentV2:
                     for card in players_hand:
                         if card == 'A':
                             has_ace += 1
-
                     usable_aces = has_ace - used_aces
+
                     obs = tuple[player_sum, dealer_hand, usable_aces, other_players_sum]
+                    action_exp = strategies.expected_value_strategy(players_hand, player_hand, dealer_hand)
+                    
+                    if action_exp == action:
+                        additional_reward = 1
+                    else: 
+                        additional_reward = -1
 
                     if action == "s":
-                        temporal_difference = (reward[0] - self.q_values[obs][action_n])
+                        action_n = 0
+                        temporal_difference = (additional_reward + reward[0] - self.q_values[obs][action_n])
+                        counter1 += 1
                     else:
+                        counter2 += 1
+                        action_n = 1
                         player_hand = final_state[0][0][0:(2+counter)]
                         player_sum , used_aces = bj.total(player_hand, True)
                         has_ace = 0
                         for card in players_hand:
                             if card == 'A':
                                 has_ace += 1
+                        usable_aces = has_ace - used_aces
                         new_obs = tuple[player_sum, dealer_hand, usable_aces, other_players_sum]
-                        temporal_difference = (reward[0] + 0.95 * np.max(self.q_values[new_obs]) - self.q_values[obs][action_n])
+
+                        reward_n = -1
+                        if counter + 1 == len(player_data[0]):
+                            reward_n = reward[0]
+                            counter3 += 1
+                        else:
+                            counter4 += 1
+                            if action_exp == "h":
+                                reward_n = 1
+
+                        temporal_difference = (additional_reward + reward_n + 0.95 * np.max(self.q_values[new_obs]) - self.q_values[obs][action_n])
                     
                     self.q_values[obs][action_n] = (self.q_values[obs][action_n] + self.lr * temporal_difference)
-                    self.decay_epsilon()
                     counter += 1
-
+                self.decay_epsilon()
+                i += 1
         print(f"Average reward during training: {total_reward/i}")
+        #print(counter1)
+        #print(counter2)
+        #print(counter3)
+        #print(counter4)
 
 if __name__ == "__main__":
-    repetitions = 5000
+    repetitions = 10000
     agent = BlackJackAgentV2(0.01, 1, 2/repetitions, 0.1)
     agent.train(repetitions)
